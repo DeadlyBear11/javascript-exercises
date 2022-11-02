e6464c2c6c203f1b3ba6c7771b80035094f9eb49
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2022-11-02 14:58:27 -0600

    Solved exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    let multipliedString = '';
+
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            multipliedString += string;
+        }
+    }
+
+    return multipliedString;
 };
 
 // Do not edit below this line
