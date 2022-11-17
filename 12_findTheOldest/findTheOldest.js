287638e198ab274485fb12c606fc57a3f72cc9a2
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2022-11-16 22:57:25 -0700

    Solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldestPerson = arr.reduce((oldest, person) => {
+        if (!person.yearOfDeath) {
+            const currentYear = (new Date()).getFullYear();
+            person.yearOfDeath = currentYear;
+        }
 
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let personAge = person.yearOfDeath - person.yearOfBirth;
+
+        if (personAge > oldestAge) {
+            oldest = person;
+        }
+
+        return oldest;
+
+    }, {
+        yearOfBirth: 1,
+        yearOfDeath: 2,
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
