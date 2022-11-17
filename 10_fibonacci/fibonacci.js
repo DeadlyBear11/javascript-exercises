f8d0931311551a8824f12fd96e61f89ec7d702ed
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2022-11-16 22:20:24 -0700

    Solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS";
+    }
+    
+    const arr = [1, 1];
+    for (let i = 1; i < position; i++) {
+        let num1 = arr[i - 1];
+        let num2 = arr[i];
+        let sum = num1 + num2;
+        arr.push(sum);
+    }
 
+    return arr[arr.length - 2];
 };
 
 // Do not edit below this line
